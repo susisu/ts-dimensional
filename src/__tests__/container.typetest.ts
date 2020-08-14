5b54dc8f37a7640f8592c744ca56477e54073d42
Susisu <susisu2413@yahoo.co.jp>
2020-08-14 21:58:53 +0900

    test: add test cases with type arguments

--- src/__tests__/container.typetest.ts
+++ src/__tests__/container.typetest.ts
@@ -1,4 +1,5 @@
-import { mass, length, mks, cgs, add, sub, mul, div } from "../container";
+import { Mass, Length } from "../dimension";
+import { mass, length, MKS, CGS, mks, cgs, add, sub, mul, div } from "../container";
 
 const m1 = mass(42, mks);
 const m2 = mass(42, cgs);
@@ -11,19 +12,35 @@ const l2 = length(42, cgs);
 // @ts-expect-error
 add(m1, m2);
 // @ts-expect-error
+add<Mass, MKS | CGS>(m1, m2);
+// @ts-expect-error
 add(m1, l1);
+// @ts-expect-error
+add<Mass | Length, MKS>(m1, l1);
 
 // @ts-expect-error
 sub(m1, m2);
 // @ts-expect-error
+sub<Mass, MKS | CGS>(m1, m2);
+// @ts-expect-error
 sub(m1, l1);
+// @ts-expect-error
+sub<Mass | Length, MKS>(m1, l1);
 
 // @ts-expect-error
 mul(m1, m2);
 // @ts-expect-error
+mul<Mass, Mass, MKS | CGS>(m1, m2);
+// @ts-expect-error
 mul(m1, l2);
+// @ts-expect-error
+mul<Mass, Length, MKS | CGS>(m1, l2);
 
 // @ts-expect-error
 div(m1, m2);
 // @ts-expect-error
+div<Mass, Mass, MKS | CGS>(m1, m2);
+// @ts-expect-error
 div(m1, l2);
+// @ts-expect-error
+div<Mass, Length, MKS | CGS>(m1, l2);
